94813246ea0e00caf1e705630ff659cf1a4a14cc
MiriamHuanca <mairim.acnauh@gmail.com>
2020-08-31 23:54:53 -0300

    Outputting Posts and Showing a Loading Indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'creating-pipe-project';
+export class AppComponent implements OnInit{
+  loadedPosts: Post[] = [];
+  isFetching = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCleatePosts(postData: Post) {
+    this.http.post<{ name: string }>('https://ng-complete-guide-c56d3.firebaseio.com/posts.json', postData).subscribe(responseData => {
+      console.log(responseData);
+    });
+  }
+
+  onFetchPosts() {
+    this.fetchPosts();
+  }
+
+  onClearPosts() {}
+
+  private fetchPosts() {
+    this.isFetching = true;
+    this.http.get<{ [key: string]: Post }>('https://ng-complete-guide-c56d3.firebaseio.com/posts.json').pipe(
+      map((responseData: { [key: string]: Post}) => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({...responseData[key], id: key});
+          }
+        }
+        return postsArray;
+      })
+    ).subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
+  }
 }
