989f479327f8b380bbd0d3193ecec773eb34be6e
MiriamHuanca <mairim.acnauh@gmail.com>
2020-09-02 22:35:31 -0300

    Using Subject for error operator

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -3,18 +3,24 @@ import { HttpClient } from "@angular/common/http";
 
 import { Post } from './post.model';
 import { map } from 'rxjs/operators';
+import { Subject } from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
+  error = new Subject<string>();
 
   constructor(private http: HttpClient) {}
 
   createAndStorePost(title: string, content: string) {
     const postData: Post = {title:title, content: content};
     this.http.post<{ name: string }>('https://ng-complete-guide-c336d.firebaseio.com/posts.json', postData
-    ).subscribe(responseData => {
-      console.log(responseData);
-    });
+    ).subscribe(
+      responseData => {
+        console.log(responseData);
+      },
+      error => {
+        this.error.next(error.message);
+      });
   }
 
   fetchPosts() {
