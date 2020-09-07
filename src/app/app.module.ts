aaac74a38c80992e36116ed686305ab6066caae9
MiriamHuanca <mairim.acnauh@gmail.com>
2020-09-07 20:59:42 -0300

    Multiple interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AuthInterceptorService } from './auth-interceptor.service';
+import { LoggingInterceptorService } from './logging-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,12 @@ import { AuthInterceptorService } from './auth-interceptor.service';
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptorService,
       multi: true //permite registrar un servicio con un identificador diferente y tener realmente multiples servicios con ese identificador 
-     }
+     },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoggingInterceptorService,
+      multi: true
+    }
     ],
   bootstrap: [AppComponent]
 })
