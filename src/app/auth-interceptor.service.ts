24ad85870c88c077a3139f77899a0dae2a80e26f
MiriamHuanca <mairim.acnauh@gmail.com>
2020-09-07 18:46:05 -0300

    Response Interceptors (used pipe)

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,8 +1,23 @@
-import { HttpInterceptor, HttpRequest, HttpHandler } from "@angular/common/http";
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEventType } from "@angular/common/http";
+import { tap } from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept (req: HttpRequest<any>, next: HttpHandler) {
-    console.log('Request i son its way');
-    return next.handle(req);
+    console.log('Request is on its way');
+    //req.url = 'a new url'//para modificar la solicitud
+    console.log(req.url);
+    const modifiedRequest = req.clone({
+      headers: req.headers.append('Auth', 'xyz')
+    });
+    return next.handle(modifiedRequest).pipe(
+      tap(event=> {
+        console.log(event);
+        if (event.type === HttpEventType.Response) {
+          console.log('Response arrived, body data: ');
+          console.log(event.body);
+        }
+      })
+    );
+    // pipe para la transformacion del body
   }
 }
